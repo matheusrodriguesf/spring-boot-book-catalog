1aa8e432e5db7b008ad41ebb375db06ccb17a722
Matheus Rodrigues <matheus.arcelino@mds.gov.br>
2026-04-16 13:34:55 -0300

    Criacao das camadas de servico

---
+++ book-catalog-api/src/main/java/br/com/arcelino/bookcatalogapi/service/LivroService.java
@@ -0,0 +1,19 @@
+package br.com.arcelino.bookcatalogapi.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.arcelino.bookcatalogapi.repository.LivroRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class LivroService {
+
+    LivroRepository livroRepository;
+
+}
