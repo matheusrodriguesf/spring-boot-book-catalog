1c10c6081e16c4a8316433db0d22729e9d2def2a
Matheus Rodrigues <matheus.arcelino@mds.gov.br>
2026-04-16 12:54:27 -0300

    Docker-compose && configuracao MARIADB

---
+++ book-catalog-api/src/main/java/br/com/arcelino/book_catalog_api/BookCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.book_catalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogApiApplication.class, args);
+	}
+
+}
---
+++ book-catalog-api/src/test/java/br/com/arcelino/book_catalog_api/BookCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.book_catalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
